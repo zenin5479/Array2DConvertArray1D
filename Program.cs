bb1c258b0ee9eec33c8544208ed78cdc3c63594a
zenin5479 <zenin5479@gmail.com>
2025-05-04 13:02:45 +0300

    Update 1.2 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -98,10 +98,40 @@ namespace Array2DConvertArray1D
       
       private static void ArrayConvertTwo()
       {
-         throw new NotImplementedException();
+         // Создание 1D - массива из 2D - массива по столбцам
+         // Создание двумерного массива из 2 строк и трех столбцов
+         int[,] int2DArray = new int[2, 3];
+         Console.WriteLine("Ввод элементов двумерного массива: ");
+         for (int i = 0; i < 2; i++)
+         {
+            for (int j = 0; j < 3; j++)
+            {
+               int2DArray[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+         }
+         int index = 0;
+         // Получение количества строк двумерного массива
+         int noOfRows = int2DArray.GetLength(0);
+         // Получение количества столбцов двумерного массива
+         int noOfColumns = int2DArray.GetLength(1);
+         // Создание трехмерного массива путем умножения значений noOfRows и noOfColumns
+         int[] oneDimensionalArray = new int[noOfRows * noOfColumns];
+
+         // Присвоение элементов 1d-массиву из 2d-массива
+         for (int y = 0; y < noOfColumns; y++)
+         {
+            for (int x = 0; x < noOfRows; x++)
+            {
+               oneDimensionalArray[index] = int2DArray[x, y];
+               index++;
+            }
+         }
+         // Печать элементов массива 1d
+         Console.WriteLine("Элементы одномерного массива: ");
+         foreach (int item in oneDimensionalArray)
+         {
+            Console.Write(item + " ");
+         }
       }
-
-
-
    }
 }
