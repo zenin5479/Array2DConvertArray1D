df8605ed7be3ed7571c807f85ef7f4693a115298
zenin5479 <zenin5479@gmail.com>
2025-05-04 12:44:46 +0300

    Update 0.1 Обновлен метод

--- Program.cs
+++ Program.cs
@@ -6,7 +6,52 @@ namespace Array2DConvertArray1D
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         int row, column, i, j, k = 0;
+         int[] array1D = new int[12];
+         int[,] array2D = new int[12, 12];
+         Console.WriteLine("Введите количество строк и столбцов");
+         int.TryParse(Console.ReadLine(), out row);
+         //row = Convert.ToInt32(Console.ReadLine());
+         int.TryParse(Console.ReadLine(), out column);
+         //column = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Введите элементы двумерного массива");
+         for (i = 0; i < row; i++)
+         {
+            for (j = 0; j < column; j++)
+            {
+               int.TryParse(Console.ReadLine(), out array2D[i, j]);
+               //array2D[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+         }
+
+         Console.WriteLine("Двумерный массив:");
+         for (i = 0; i < row; i++)
+         {
+            for (j = 0; j < column; j++)
+            {
+               //Console.WriteLine("[{0},{1}] = {2}", i, j, array2D[i, j]);
+               Console.Write("[{0}]", array2D[i, j]);
+            }
+
+            Console.WriteLine();
+         }
+
+         Console.WriteLine("Одномерный массив:");
+         for (i = 0; i < row; i++)
+         {
+            for (j = 0; j < column; j++)
+            {
+               array1D[k++] = array2D[i, j];
+            }
+         }
+
+         for (i = 0; i < row * column; i++)
+         {
+            //Console.WriteLine("[{0}] = {1}", i, array1D[i]);
+            Console.Write("[{0}]", array1D[i]);
+         }
+
+         Console.ReadLine();
       }
    }
 }
